f933011647de59293c001e27a7b52491c49375c2
Mahmudur Rahman Promise <mahmudur.rahman.csedu@gmail.com>
2023-11-24 16:15:31 +0600

    add google authentication

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,17 +7,18 @@ import {signIn, signOut, useSession, getProviders} from 'next-auth/react';
 
 const Nav = () => {
     const isUserLoggedIn = true;
+    const {data: session} = useSession();
 
     const [providers, setProviders] = useState(null);
     const [toggleDown, setToggleDown] = useState(false)
 
     useEffect(() => {
-        const setProviders = async () => {
+        const setUpProviders = async () => {
             const response = await getProviders();
             setProviders(response);
         }
 
-        setProviders();
+        setUpProviders()
     }, [])
 
     return (
@@ -35,7 +36,7 @@ const Nav = () => {
 
             {/*desktop navigation*/}
             <div className="sm:flex hidden">
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className="flex gap-3 md:gap-5">
                         <Link href="/create-prompt" className="black_btn">
                             Create Post
@@ -47,7 +48,7 @@ const Nav = () => {
 
                         <Link href="/profile">
                             <Image
-                                src="/assets/images/logo.svg"
+                                src={session?.user.image}
                                 alt="profile"
                                 width={37}
                                 height={37}
@@ -74,10 +75,10 @@ const Nav = () => {
 
             {/*mobile navigation*/}
             <div className="sm:hidden flex relative">
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className="flex">
                         <Image
-                            src="/assets/images/logo.svg"
+                            src={session?.user.image}
                             alt="Profile"
                             width={37}
                             height={37}
